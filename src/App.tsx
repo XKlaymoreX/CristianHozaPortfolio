43de6da8c418f3dd2a2dc251ea5a69d41a1cb461
Cristian Hoza <cristianhoza@MacBook-Pro-di-Cristian.local>
2021-08-13 13:08:45 +0200

    Added LightModeSwitch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './Home.css';
-import BackgroundDetail from './Components/BackgroundDetail';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import React from 'react';
@@ -8,34 +7,44 @@ gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
 
-  useEffect(() => {
 
+  const [isNightModeOn, setNightMode] = useState(false)
+
+  useEffect(() => {
     gsap.fromTo('.landingSection' , {
       opacity:0
     }, {opacity:1, duration:.8})
-
-
-
   }, [])
 
 
+  const lightModeSwitchAnimationTrigger = () => {
+    gsap.fromTo('.landingSection' , {
+      opacity:0
+    }, {opacity:1, duration:.8})
+  }
+
+  
+
   return (
-    <div className="Home">
+    <div className={isNightModeOn ? "HomeNM" : "Home"}>
       <div className="landingSection">
 
         {/* IMAGE THUMBNAIL */}
         <div className="profileImageThumbnail centered">
-          <div className="profileImage"></div>
+          <div className={isNightModeOn ? "profileImageNM" : "profileImage"}></div>
           <div className="profileImageFlag">🇨🇭</div>
         </div>
         <p>HI THERE 👋🏻 I'M</p>
         <span>Cristian Hoza</span>
-        <div className="myProfession">full stack developer & <br /> web design enthusiast</div>
-        <div className="myProfessionDetailed">I’m a specialist Full Stack Developer<br/>
+        <div className={isNightModeOn ? "myProfessionNM" : "myProfession"}>full stack developer & <br /> web design enthusiast</div>
+        <div className={isNightModeOn ? "myProfessionDetailedNM" : "myProfessionDetailed"}>I’m a specialist Full Stack Developer<br/>
           with growing UI/UX Skills,<br/>
           based in Switzerland.
         </div>
-        <div className="callToAction centered">check my work</div>
+        <div className={`centered ${isNightModeOn ? "callToActionNM" : "callToAction"}`}>check my work</div>
+
+        <input type="checkbox" style={{cursor:'pointer'}} onClick={() => {setNightMode(!isNightModeOn); lightModeSwitchAnimationTrigger()}}></input>
+        {isNightModeOn ? <div>Night Mode On</div> : <div>Night Mode Off</div>}
       </div>
     </div>
   );
